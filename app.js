8ad2757d651e032071d4c3486d2c37c5d551c665
abhilashdzr <abhilashxaviers@gmail.com>
2022-05-26 16:02:14 +0530

    unstringified json

--- app.js
+++ app.js
@@ -48,9 +48,9 @@ app.get("/search", function (req, res) {
     // with arguments and send this data to res object
     process.stdout.on("data", function (data) {
       console.log("Sending Info")
-      mystr = data.toString("utf8")
+      // mystr = data.toString("utf8")
 
-      myjson = JSON.parse(mystr)
+      myjson = JSON.parse(data)
       res.json(myjson)
     })
   }
